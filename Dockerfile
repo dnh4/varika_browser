35a2a285622fca1a4146996bf84cf4dd9997acc0
Dinahasina <dina@atscom.io>
2023-08-30 18:10:30 +0300

    Install cargo web for our docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM rust:latest
 RUN apt-get update && apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev
+RUN cargo install cargo-web
 RUN mkdir -pv /app
 WORKDIR /app
 COPY . .
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate web_view;
+
+use web_view::WebView;
+
 fn main() {
-    println!("Hello, world!");
+    web_view::builder()
+        .title("Webview Example")
+        .content(web_view::Content::Html("<html><body><h1>Hello, WebView!</h1></body></html>"))
+        .size(800, 600)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
 }
